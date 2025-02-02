8a5f1d0a7f3feeb74f9dd300d6f815de4ccd8037
jao <silvrt3d@mail.com>
2025-02-02 07:34:22 -0300

    Adicionado nome de domínio

--- Provedor/Containers/WEBMAIL/Config/config.inc.php
+++ Provedor/Containers/WEBMAIL/Config/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 $config['imap_host'] = 'tls://email:143';
-$config['smtp_host'] = 'tls://email:587';
+$config['smtp_host'] = 'tls://email:25';
 #$config['username_domain'] = 'praia.domeio.com';
 $config['mail_domain'] = 'praia.domeio.com';
 $config['imap_conn_options'] = [
