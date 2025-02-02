28584e8d2821f1aa7adddee09d57278cc50ae539
jao <silvrt3d@mail.com>
2025-02-02 08:05:42 -0300

    Adicionado nome de domínio

--- Provedor/Containers/WEBMAIL/Config/config.inc.php
+++ Provedor/Containers/WEBMAIL/Config/config.inc.php
@@ -1,9 +1,10 @@
 <?php
-$config['smtp_server'] = 'tls://mail.praia.domeio.com'; // ou "email" (nome do serviço no Docker)
-$config['smtp_port'] = 587; // ou 25 (sem TLS)
-$config['smtp_user'] = '%u'; // Usa o mesmo usuário do IMAP
-$config['smtp_pass'] = '%p'; // Usa a mesma senha do IMAP
-$config['smtp_conn_options'] = [
+$config['imap_host'] = 'tls://email:143';
+$config['smtp_host'] = 'tls://email:25';
+$config['smtp_server'] = 'tls://email:587';
+#$config['username_domain'] = 'praia.domeio.com';
+$config['mail_domain'] = 'praia.domeio.com';
+$config['imap_conn_options'] = [
   'ssl' => [
     'verify_peer' => false,
     'verify_peer_name' => false,
