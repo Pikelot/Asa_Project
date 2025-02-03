03e3307e159900fb3fcb41a3a6b6ae0c58f271ac
jao <silvrt3d@mail.com>
2025-02-03 17:46:23 -0300

    configuração de teste para compose 5

--- Provedor/Containers/WEBMAIL/Config/config.inc.php
+++ Provedor/Containers/WEBMAIL/Config/config.inc.php
@@ -5,10 +5,10 @@ $config['smtp_port'] = 587;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 $config['mail_domain'] = 'praia.domeio.com';
-$config['imap_conn_options'] = [
-  'ssl' => [
-    'verify_peer' => false,
-    'verify_peer_name' => false,
-    'allow_self_signed' => true
-  ]
-];
+$config['smtp_conn_options'] = array(
+  'ssl' => array(
+      'verify_peer' => false,
+      'verify_peer_name' => false,
+      'allow_self_signed' => true
+  )
+);
