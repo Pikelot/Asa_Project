87b882cd0f5073412e02cdb8ffdd58eb8fed78f9
jao <silvrt3d@mail.com>
2025-02-03 18:52:37 -0300

    socorro

--- Provedor/Containers/WEBMAIL/Config/config.inc.php
+++ Provedor/Containers/WEBMAIL/Config/config.inc.php
@@ -1,14 +1,14 @@
 <?php
 $config['imap_host'] = 'tls://email:143';
-$config['smtp_host'] = 'tls://email:25';
-#$config['smtp_port'] = 587;
+$config['smtp_host'] = 'tls://email:587';
+$config['smtp_port'] = 587;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 $config['mail_domain'] = 'praia.domeio.com';
-$config['smtp_conn_options'] = array(
-  'ssl' => array(
-      'verify_peer' => false,
-      'verify_peer_name' => false,
-      'allow_self_signed' => true
-  )
-);
+$config['imap_conn_options'] = [
+  'ssl' => [
+    'verify_peer' => false,
+    'verify_peer_name' => false,
+    'allow_self_signed' => true
+  ]
+];
