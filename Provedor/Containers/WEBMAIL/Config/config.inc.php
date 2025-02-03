20a5a925ed8b3c0c75e87865957a9941d67138f8
jao <silvrt3d@mail.com>
2025-02-03 19:05:49 -0300

    socorro

--- Provedor/Containers/WEBMAIL/Config/config.inc.php
+++ Provedor/Containers/WEBMAIL/Config/config.inc.php
@@ -6,16 +6,10 @@ $config['smtp_auth_type'] = '';
 $config['smtp_user'] = ''; 
 $config['smtp_pass'] = '';
 $config['mail_domain'] = 'praia.domeio.com';
-$config['imap_conn_options'] = [
-  'ssl' => [
-    'verify_peer' => false,
-    'verify_peer_name' => false,
-    'allow_self_signed' => true
-  ]
-];
-
-$config['smtp_server'] = 'localhost';
-$config['smtp_port'] = 25; 
-$config['smtp_auth_type'] = ''; 
-$config['smtp_user'] = ''; 
-$config['smtp_pass'] = '';
+$config['smtp_conn_options'] = array(
+  'ssl' => array(
+      'verify_peer' => false,
+      'verify_peer_name' => false,
+      'allow_self_signed' => true
+  )
+);
