4c4686ffbd62adb56a55e71a1b785a5b160952f8
jao <silvrt3d@mail.com>
2025-02-03 17:50:46 -0300

    configuração de teste para compose 5.1

--- Provedor/Containers/WEBMAIL/Config/config.inc.php
+++ Provedor/Containers/WEBMAIL/Config/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 $config['imap_host'] = 'tls://email:143';
-$config['smtp_host'] = 'tls://email:587';
-$config['smtp_port'] = 587;
+$config['smtp_host'] = 'tls://email:25';
+#$config['smtp_port'] = 587;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 $config['mail_domain'] = 'praia.domeio.com';
