102454be2506cb5f36eafa85332e498b9c2d60a8
jao <silvrt3d@mail.com>
2025-02-03 19:48:30 -0300

    mailbox conf 1.5

--- Provedor/Containers/WEBMAIL/Config/config.inc.php
+++ Provedor/Containers/WEBMAIL/Config/config.inc.php
@@ -2,9 +2,9 @@
 $config['imap_host'] = 'tls://email:143';
 $config['smtp_server'] = 'localhost';
 $config['smtp_port'] = 25; 
-$config['smtp_auth_type'] = ''; 
-$config['smtp_user'] = ''; 
-$config['smtp_pass'] = '';
+$config['smtp_auth_type'] = 'PLAIN'; 
+$config['smtp_user'] = '%u'; 
+$config['smtp_pass'] = '%p';
 $config['mail_domain'] = 'praia.domeio.com';
 $config['imap_conn_options'] = [
   'ssl' => [
