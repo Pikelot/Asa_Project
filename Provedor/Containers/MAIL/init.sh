1d24d86c919dfbf9cc3ae2de0e0956b2c0fb9558
jao <silvrt3d@mail.com>
2025-02-04 18:45:02 -0300

    Versão corrigida final mailbox

--- Provedor/Containers/MAIL/init.sh
+++ Provedor/Containers/MAIL/init.sh
@@ -3,13 +3,14 @@
 # Verifica se os Maildirs já existem no volume, se não, copia do backup interno
 if [ ! -d "/home/usuario/Maildir" ]; then
     cp -r /maildata/usuario /home/
-    chown -R usuario:usuario /home/usuario
 fi
 
 if [ ! -d "/home/usuario2/Maildir" ]; then
     cp -r /maildata/usuario2 /home/
-    chown -R usuario2:usuario2 /home/usuario2
 fi
+
+chown -R usuario:usuario /home/usuario
+chown -R usuario2:usuario2 /home/usuario2
 
 # Iniciar serviços
 service syslog-ng start
